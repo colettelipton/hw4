72a12023770267730cc960b45d4c0643f0deaf43
Colette Lipton <clipton@usc.edu>
2023-03-20 23:07:08 +0000

    Tree traversals done!

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,18 +1,41 @@
 #ifndef RECCHECK
-//if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <iostream>
 #endif
 
 #include "equal-paths.h"
+#include <algorithm>
 using namespace std;
 
-
-// You may add any prototypes of helper functions here
-
+bool treelen(int curlen, int& fpath, Node* root);
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
-
+	int first = -1;
+	return treelen(0, first, root);
 }
 
+bool treelen(int curlen, int& fpath, Node* root){
+	if(root == nullptr){
+		return true;
+	}
+	if(root->left == nullptr && root->right == nullptr){
+		if(fpath == -1){
+			fpath = curlen;
+			return true;
+		}
+
+		else{
+			return fpath == curlen;
+		}
+	}
+	
+	// recurse down left subtree
+	bool left = treelen(curlen + 1, fpath, root->left);
+
+	// recurse down right subtree
+	bool right = treelen(curlen + 1, fpath, root->right);
+
+	if(left && right) return true;
+	else return false;
+	
+}
